b4a5bb7e8552f8ee3a093f119077228d2eb10080
haroonbajwa <haroonbajwa99@gmail.com>
2025-03-30 02:21:01 +0500

    initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
